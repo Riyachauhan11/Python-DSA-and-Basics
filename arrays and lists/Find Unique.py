6b7f99d766d9fcce61f2da0abfbb7db3ab4ce15b
Riya Chauhan <96919050+reticent11@users.noreply.github.com>
2022-10-04 14:03:52 +0530

    Update Find Unique.py

--- arrays and lists/Find Unique.py
+++ arrays and lists/Find Unique.py
@@ -1,7 +1,12 @@
 #You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
 #Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
 #You need to find and return that number which is unique in the array/list.
-#t refers to test cases and in input array 1 Unique element is always present.
+#in input array 1 Unique element is always present.
+
+#The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+#First line of each test case or query contains an integer 'N' representing the size of the array/list.
+#Second line contains 'N' single space separated integers representing the elements in the array/list.
+
 
 def findUnique(arr, n) :
     #Your code goes here
