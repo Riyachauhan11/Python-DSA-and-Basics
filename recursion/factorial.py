fbad310e8741ca8d6e0182efce24eb1dc4e082ad
Riya Chauhan <96919050+reticent11@users.noreply.github.com>
2022-10-11 22:25:44 +0530

    Create factorial.py

---
+++ recursion/factorial.py
@@ -0,0 +1,16 @@
+#to avoid max recursion depth exceeded error in our program
+#when we input large numbers, we can use the following
+import sys
+sys.setrecursionlimit(10000)
+
+def fact(n):
+    if n==0:
+        return 1
+    return n*fact(n-1)
+
+n=int(input())
+print(fact(n))
+
+'''base case : n=0
+hypothesis : fact(n-1) {if this works then induction step will def work}
+induction step : fact(n)'''
