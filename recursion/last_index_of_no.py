12a1c9e1b95b2181ad4b88697018867f267122f6
Riya Chauhan <96919050+reticent11@users.noreply.github.com>
2022-10-14 23:30:29 +0530

    Update last_index_of_no.py

--- recursion/last_index_of_no.py
+++ recursion/last_index_of_no.py
@@ -1,3 +1,11 @@
+#Given an array of length N and an integer x, you need to find 
+#and return the last index of integer x present in the array.
+#Return -1 if it is not present in the array.
+#Last index means - if x is present multiple times in the array, return the index at which x comes last in the array.
+#You should start traversing your array from 0, not from (N - 1).
+#Do this recursively. Indexing in the array starts from 0.
+
+'''using list slicing'''
 def last_Index(arr,x):
     l=len(arr)
     if l==0:
@@ -19,6 +27,8 @@ arr=list(int(i) for i in input().strip().split(' '))
 x=int(input())
 print(last_Index(arr, x))
 
+
+'''using starting index (better solution - explained why in check_sorted_list.py)'''
 def last_Index(arr,x,si):
     l=len(arr)
     if si==l:
@@ -31,7 +41,7 @@ def last_Index(arr,x,si):
             return si
         else:
             return -1
-
+        
 # Main
 from sys import setrecursionlimit
 setrecursionlimit(11000)
