186bd3daf12021f467cef781a84925c60d93ce28
Riya Chauhan <96919050+reticent11@users.noreply.github.com>
2022-10-14 23:23:19 +0530

    Update check_sorted_list.py

--- recursion/check_sorted_list.py
+++ recursion/check_sorted_list.py
@@ -1,3 +1,6 @@
+#check whether the list is sorted or not recursively
+
+'''using list slicing'''
 def isSorted(li):
     if len(li)==1 or len(li)==0:
         return True
@@ -7,8 +10,11 @@ def isSorted(li):
     return isSorted(smaller_list)
 print(isSorted([1,2,3,4,5,7,6]))
 
-#better solution
 
+#better solution as the former code will require a big runtime in case we give it a big list as it will be creating multiple large copies of that big list
+#while the following code will require less runtime as its running through only one list
+
+'''using starting index'''
 def isSorted_better(li,si=0):
     if si==len(li)-1 or si==len(li):
         return True
