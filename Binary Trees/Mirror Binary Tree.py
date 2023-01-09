c00bd8f4d448b5a79fda4c409864a073fd07561e
Riya Chauhan <96919050+Riyachauhan11@users.noreply.github.com>
2023-01-10 03:07:16 +0530

    Create Mirror Binary Tree.py

---
+++ Binary Trees/Mirror Binary Tree.py
@@ -0,0 +1,98 @@
+'''For a given Binary Tree of type integer, update it with its corresponding mirror image.'''
+
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+
+def mirrorBinaryTree(root) :
+    # Your code goes here
+    if root==None:
+        return 
+
+    mirrorBinaryTree(root.left)
+    mirrorBinaryTree(root.right)
+
+    root.left,root.right=root.right,root.left
+
+
+
+
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    
+    length = len(levelOrder)
+
+    if length == 1 :
+        return None
+    
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+	
+def printLevelWise(root):
+    if root==None:
+        return
+
+    inputQ = queue.Queue()
+    outputQ = queue.Queue()
+    inputQ.put(root)
+
+    while not inputQ.empty():
+       
+        while not inputQ.empty():
+       
+            curr = inputQ.get()
+            print(curr.data, end=' ')
+            if curr.left!=None:
+                outputQ.put(curr.left)
+            if curr.right!=None:
+                outputQ.put(curr.right)
+       
+        print()
+        inputQ, outputQ = outputQ, inputQ
+
+
+# Main
+root = takeInput()
+
+mirrorBinaryTree(root)
+printLevelWise(root)
