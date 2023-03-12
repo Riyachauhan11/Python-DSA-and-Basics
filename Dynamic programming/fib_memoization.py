fb5d05ff8ff34fd4c3deb0c9f5d1595a7faa44bb
Riya Chauhan <96919050+Riyachauhan11@users.noreply.github.com>
2023-03-12 23:43:22 +0530

    Create fib_memoization.py

---
+++ Dynamic programming/fib_memoization.py
@@ -0,0 +1,39 @@
+# memoization is done to reduce overlapping subproblems
+'''overlapping subproblems include calling recursion on arguments we have
+already called recursion on so to avoid this memoization is done'''
+
+def fibR(n,dp):
+    if n==0 or n==1:
+        return n
+    
+    if dp[n-1]==-1:
+        ans1=fibR(n-1,dp)
+        dp[n-1]=ans1
+    else:
+        ans1=dp[n-1]
+    
+    if dp[n-2]==-1:
+        ans2=fibR(n-2,dp)
+        dp[n-2]=ans2
+    else:
+        ans2=dp[n-2]
+
+    return ans1+ans2
+
+
+def fibI(n):
+    dp=[0 for i in range(n+1)]
+    dp[0]=0
+    dp[1]=1
+    i=2
+    while i<=n:
+        dp[i]=dp[i-1]+dp[i-2]
+        i+=1
+    return dp[n]
+
+
+n=int(input())
+ans=fibI(n)
+'''dp=[-1 for i in range(n+1)]
+ans=fibR(n,dp)'''
+print(ans)
