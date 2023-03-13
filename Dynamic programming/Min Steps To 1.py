5235e8a0acf1fe41762eeab823901444eb03e9fc
Riya Chauhan <96919050+Riyachauhan11@users.noreply.github.com>
2023-03-13 20:46:20 +0530

    Update Min Steps To 1.py

--- Dynamic programming/Min Steps To 1.py
+++ Dynamic programming/Min Steps To 1.py
@@ -83,6 +83,30 @@ def countMinStepsToOne(n,dp) :
 
     return ans
   
+    
+def countMinStepsToOneI(n):
+    dp=[-1 for i in range(n+1)]
+    dp[1]=0
+    i=2
+    while i<=n:
+        ans1=dp[i-1]
+
+        if i%2==0:
+            ans2=dp[i//2]
+        else:
+            ans2=sys.maxsize
+        
+        if i%3==0:
+            ans3=dp[i//3]
+        else:
+            ans3=sys.maxsize
+        
+        dp[i]=1+min(ans1,ans2,ans3)
+        i+=1
+    
+    return dp[n]
+
+        
 
 #main
 n = int(stdin.readline().rstrip())
