44188d1c42ba768d19aedbdc70e778eb7143abdc
Riya Chauhan <96919050+Riyachauhan11@users.noreply.github.com>
2023-04-09 22:17:23 +0530

    Create creating_graph.py

---
+++ Graphs/creating_graph.py
@@ -0,0 +1,32 @@
+#creating graph
+#using adjacency matrix (one of the methods to create graph)
+class Graph():
+    def __init__(self,nVertices):
+        self.nVertices=nVertices
+        self.adjMatrix=[[0 for i in range(nVertices)] for j in range(nVertices)]
+
+    def addEdge(self,v1,v2):
+        self.adjMatrix[v1][v2]=1
+        self.adjMatrix[v2][v1]=1
+
+    def removeEdge(self,v1,v2):
+        if self.containsEdge(v1,v2) is False:
+            return
+        self.adjMatrix[v1][v2]=0
+        self.adjMatrix[v2][v1]=0
+
+    def containsEdge(self,v1,v2):
+        if self.adjMatrix[v1][v2]>0:
+            return True
+        else:
+            return False
+        
+    #changs the deafult str that gets printed when calling print(g)
+    def __str__(self):
+        return str(self.adjMatrix)
+
+g=Graph(5)
+g.addEdge(0,1)
+g.addEdge(1,3)
+g.addEdge(2,4)
+print(g)
